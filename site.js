5f40f70a00710797926aa981df843831d59ddc70
davonab <davon.branch42@yahoo.com>
2018-02-17 15:21:31 -0600

    remove unncessary escape characters to comply with linter

--- site.js
+++ site.js
@@ -5,26 +5,26 @@
 /* eslint-enable */
 $.noConflict();
 (function($) {
-  //validate user email
+  // validate user email
   var userEmail = $('#email').val();
-  var validateEmail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  var validateEmail = /^([a-zA-Z0-9_])+(([a-zA-Z0-9])+)+([a-zA-Z0-9]{2,4})+$/;
   $('#signup-form').on('submit', function(d) {
     d.preventDefault();
-    //check if the name input box is empty
+    // check if the name input box is empty
     if(document.getElementById("name").value === '' ){
       $('#name').after('<li id="error">You have missing information!</li>');
     }
-    //check if the email input box is empty
+    // check if the email input box is empty
     if(document.getElementById("email").value === '' ){
       $('#email').after('<li id="error">You have missing information!</li>');
     }
-    //check if there is data in the input boxes
+    // check if there is data in the input boxes
     if(document.getElementById("name").value !== '' && document.getElementById("email").value !== ''){
       $('#signup-form').append('<p>Your information has been submitted, thank you!</p>');
-      //remove the error messages
+      // remove the error messages
       document.getElementById("error").remove();
     }
-    //validate users email
+    // validate users email
     if(document.getElementById("email").value !== '' && !validateEmail.test(userEmail))
     {
       $('#email').append('Your email seems to be incorrect, try again');
